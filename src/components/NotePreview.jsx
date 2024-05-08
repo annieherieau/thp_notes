85c828dae6def8a9f44ccbfee32d595567bc8ed3
annieherieau <annie.herieau@gmail.com>
2024-05-08 14:23:10 +0200

    delete dans le menu

--- src/components/NotePreview.jsx
+++ src/components/NotePreview.jsx
@@ -1,10 +1,14 @@
-export default function NotePreview({ note, onClick }) {
+import { Button } from "antd";
+import {DeleteOutlined} from "@ant-design/icons"
+
+export default function NotePreview({ note, onClick, onDelete }) {
   const content = note.content.split(" ").slice(0, 14).join(" ");
   const title = note.title ? note.title : '[nouvelle note]';
   return (
     <div className="NotePreview" onClick={onClick}>
       <div className="title-preview">{title}</div>
       <div className="content-preview">{content}</div>
+      <Button className='delete-btn' icon={<DeleteOutlined />} onClick={onDelete}></Button>
     </div>
   );
 }
